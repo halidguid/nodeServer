24058450d14cd8462883f8ae299539c5b6b16e68
halidguid <halidhosi@gmail.com>
2021-04-24 01:07:17 +0200

    prvialnajjaci

---
+++ src/controller.js
@@ -0,0 +1,20 @@
+
+const root = (req, res) => {
+    console.log(req.url);
+    res.end('Hello Node Server, how are you!');
+}
+
+
+const text = (req, res) => {
+
+    const param = req.params.param || 'Text';
+    res.end(`Hello ${param} Server!`);
+}
+
+const html = (req, res) => {
+
+
+    const param = req.params.param || 'Html';
+    res.end(`<h1>Hello ${param} Server!</h1>`);
+}
+export  { root, text, html }
